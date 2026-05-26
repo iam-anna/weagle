4edaa655fc3b9d87a477507be4d6eeda2c43e176
Arthur Francisco Barcala Rodrigues Silva <arthur.barcala@institutojef.org.br>
2026-05-26 10:38:19 -0300

    listagem de projetos

--- app/src/main/java/com/fiapos/weagle/features/projects/data/ProjectRepository.kt
+++ app/src/main/java/com/fiapos/weagle/features/projects/data/ProjectRepository.kt
@@ -2,12 +2,53 @@ package com.fiapos.weagle.features.projects.data
 
 import com.fiapos.weagle.features.projects.data.dao.ProjectDao
 import com.fiapos.weagle.features.projects.data.domain.Project
+import com.fiapos.weagle.features.projects.data.domain.ProjectStatus
 import com.fiapos.weagle.features.projects.data.mappers.toProject
+import java.time.LocalDate
 
 class ProjectRepository(
     private val dao: ProjectDao
 ) {
-    private val projects = mutableListOf<Project>()
+    private val projects = mutableListOf<Project>(
+// TODO: Deletar esses mocks
+//        Project(
+//            id = "1",
+//            name = "aaa",
+//            description = "dsf",
+//            status = ProjectStatus.ACTIVE,
+//            startDate = LocalDate.now(),
+//            endDate = LocalDate.now(),
+//            investment = 2000.3f,
+//            ideaList = listOf(),
+//            ownedBy = "FULANO DE tal",
+//            createdAt = LocalDate.now()
+//        ),
+//        Project(
+//            id = "1",
+//            name = "aaa",
+//            description = "dsf",
+//            status = ProjectStatus.ACTIVE,
+//            startDate = LocalDate.now(),
+//            endDate = LocalDate.now(),
+//            investment = 2000.3f,
+//            ideaList = listOf(),
+//            ownedBy = "FULANO DE tal",
+//            createdAt = LocalDate.now()
+//        ),
+//        Project(
+//            id = "1",
+//            name = "aaa",
+//            description = "dsf",
+//            status = ProjectStatus.ACTIVE,
+//            startDate = LocalDate.now(),
+//            endDate = LocalDate.now(),
+//            investment = 2000.3f,
+//            ideaList = listOf(),
+//            ownedBy = "FULANO DE tal",
+//            createdAt = LocalDate.now()
+//        )
+    )
+
 
     fun createProject(project: Project): Project {
         projects.add(project)
@@ -15,10 +56,11 @@ class ProjectRepository(
     }
 
     suspend fun getProjects(): List<Project> {
-
-        return dao.getAll()
-            .map {
-                it.toProject()
-            }
+        return projects
+// TODO: Remover mock e integrar com banco
+//        return dao.getAll()
+//            .map {
+//                it.toProject()
+//            }
     }
 }
--- app/src/main/java/com/fiapos/weagle/features/projects/presentation/listview/ListViewProjectsScreen.kt
+++ app/src/main/java/com/fiapos/weagle/features/projects/presentation/listview/ListViewProjectsScreen.kt
@@ -1,18 +1,24 @@
 package com.fiapos.weagle.features.projects.presentation.listview
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.fiapos.weagle.presentation.components.ProjectItem
 import com.fiapos.weagle.presentation.components.TopNavigation
+import com.fiapos.weagle.presentation.navigation.Routes
 
 @Composable
 fun ListViewProjectsScreen(
@@ -29,7 +35,7 @@ fun ListViewProjectsScreen(
             .padding(vertical = 64.dp, horizontal = 24.dp)
     ) {
         TopNavigation(
-            title = "Ideias",
+            title = "Projetos",
             onBackPressed = {
                 navigationController.popBackStack();
             }
@@ -55,7 +61,21 @@ fun ListViewProjectsScreen(
                 color = MaterialTheme.colorScheme.secondary
             )
         } else {
-            // TODO: project list here
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(projects) { project ->
+                    ProjectItem(
+                        project = project,
+                        onClick = {
+                            navigationController.navigate("${Routes.VIEW_PROJECT}/${project.id}")
+                        }
+                    )
+                }
+            }
         }
     }
 }
